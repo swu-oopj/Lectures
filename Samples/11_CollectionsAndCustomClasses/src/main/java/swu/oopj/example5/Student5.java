4a626379311646a6b006a32064eabf79ec1b219e
Boris Milasinovic <boris.milasinovic@fer.hr>
2019-06-25 17:20:45 +0200

    Student classes - inheritance from version to version

--- Samples/11_CollectionsAndCustomClasses/src/main/java/swu/oopj/example5/Student5.java
+++ Samples/11_CollectionsAndCustomClasses/src/main/java/swu/oopj/example5/Student5.java
@@ -1,25 +1,13 @@
 package swu.oopj.example5;
 
-import swu.oopj.Student;
+import swu.oopj.example4.Student4;
 
-public class Student5 extends Student implements Comparable<Student5> {	
+public class Student5 extends Student4 implements Comparable<Student5> {	
 	
 	public Student5(String lastName, String firstName, String studentID) {
 		super(lastName, firstName, studentID);	
 	}	
-	
-	@Override
-	public boolean equals(Object obj) {
-		if(!(obj instanceof Student5)) return false;
-		Student5 other = (Student5)obj;
-		return this.studentID.equals(other.studentID);
-	}
-	
-	@Override
-	public int hashCode(){
-		return this.studentID.hashCode();
-	}
-	
+		
     @Override
     public int compareTo(Student5 other) {
       return this.studentID.compareTo(other.studentID);
--- Samples/11_CollectionsAndCustomClasses/src/main/java/swu/oopj/example8/Student8.java
+++ Samples/11_CollectionsAndCustomClasses/src/main/java/swu/oopj/example8/Student8.java
@@ -2,35 +2,17 @@ package swu.oopj.example8;
 
 import java.util.Comparator;
 
-import swu.oopj.Student;
 import swu.oopj.example5.Student5;
 
-public class Student8 extends Student implements Comparable<Student8> {	
+public class Student8 extends Student5 {	
 	
 	public Student8(String lastName, String firstName, String studentID) {
 		super(lastName, firstName, studentID);	
 	}		
 	
-	@Override
-	public boolean equals(Object obj) {
-		if(!(obj instanceof Student8)) return false;
-		Student8 other = (Student8)obj;
-		return this.studentID.equals(other.studentID);
-	}
-	
-	@Override
-	public int hashCode(){
-		return this.studentID.hashCode();
-	}
-
-	@Override
-	public int compareTo(Student8 o) {
-		return this.studentID.compareTo(o.studentID);
-	}	
-	
 	public static final Comparator<Student8> BY_LAST_NAME = (s1,s2) -> s1.lastName.compareTo(s2.lastName);
 	//public static final Comparator<Student> BY_FIRST_NAME = (s1,s2) -> s1.firstName.compareTo(s2.firstName);
-        public static final Comparator<Student8> BY_FIRST_NAME = Comparator.comparing(Student8::getFirstName);
+    public static final Comparator<Student8> BY_FIRST_NAME = Comparator.comparing(Student8::getFirstName);
 	public static final Comparator<Student8> BY_STUDENT_ID = (s1,s2) -> s1.studentID.compareTo(s2.studentID);
 
 	    
